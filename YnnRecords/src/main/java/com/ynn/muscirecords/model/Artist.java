8566b7402dfea6bdcf80b83f639dc7a21b06e927
lukaradic <lukaradic911@gmail.com>
2020-11-26 18:25:15 +0100

    Created models Role, Track and User

--- YnnRecords/src/main/java/com/ynn/muscirecords/model/Artist.java
+++ YnnRecords/src/main/java/com/ynn/muscirecords/model/Artist.java
@@ -2,13 +2,49 @@ package com.ynn.muscirecords.model;
 
 import java.io.Serializable;
 
+
 public class Artist implements Serializable {
-	
-	
+
 	private int artistId;
 	private String artistName;
 	private String artistDescription;
 	private int artistFormationYear;
+	
+	public Artist() {}
+
+	public int getArtistId() {
+		return artistId;
+	}
+
+	public void setArtistId(int artistId) {
+		this.artistId = artistId;
+	}
+
+	public String getArtistName() {
+		return artistName;
+	}
+
+	public void setArtistName(String artistName) {
+		this.artistName = artistName;
+	}
+
+	public String getArtistDescription() {
+		return artistDescription;
+	}
+
+	public void setArtistDescription(String artistDescription) {
+		this.artistDescription = artistDescription;
+	}
+
+	public int getArtistFormationYear() {
+		return artistFormationYear;
+	}
+
+	public void setArtistFormationYear(int artistFormationYear) {
+		this.artistFormationYear = artistFormationYear;
+	}
+	
+	
 	
 	
 
