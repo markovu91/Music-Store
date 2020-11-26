6bb748fed2ce812fdfbf5eff07661fcb67fbb7fa
lukaradic <lukaradic911@gmail.com>
2020-11-26 17:40:57 +0100

    no message

---
+++ YnnRecords/src/main/java/com/ynn/muscirecords/YnnRecordsApplication.java
@@ -0,0 +1,13 @@
+package com.ynn.muscirecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YnnRecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YnnRecordsApplication.class, args);
+	}
+
+}
