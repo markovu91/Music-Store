728aea22d1a409ae5b9d9d0afb68e93a42e34fe4
lukaradic <lukaradic911@gmail.com>
2020-12-06 16:08:13 +0100

    Update - 6.12.2020. - Misc.

--- YnnRecords/src/main/java/com/ynn/muscirecords/controllers/HomeController.java
+++ YnnRecords/src/main/java/com/ynn/muscirecords/controllers/HomeController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ynn.muscirecords.model.Album;
@@ -20,14 +21,22 @@ public class HomeController {
 	
 	
 	@RequestMapping("/")
-	public String home(Model model) {
+	public String home() {
+	
+		return "index";
+	}
+	
+	@GetMapping("/store")
+	public String store(Model model) {
 		List<Album> albums = new ArrayList<>();
 		albums = albumService.getAllAlbums();
 		Album album = new Album();
 		Artist artist = album.getArtist();
 		model.addAttribute("albums", albums);
 		model.addAttribute("artist", artist);
-		return "index";
+		
+		return "store";
+		
 	}
 	
 	
